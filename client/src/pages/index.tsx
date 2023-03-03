1fcadd8a3d77361418a698d181ca45a3d7d75334
Jin Hyoung Joo <iamjinhyoungjoo@gmail.com>
2023-03-03 17:50:29 +0900

    Added header

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,22 @@
+import Container from "@mui/material/Container";
+import { Grid } from "@mui/material";
+import Header from "@/components/Header";
+
+const sections = [
+  { title: "About 혈통", url: "#" },
+  { title: "역대 공연 리스트", url: "#" },
+  { title: "역대 노래 리스트", url: "#" },
+  { title: "역대 팀 리스트", url: "#" },
+  { title: "가입 문의", url: "#" },
+];
+
 export default function Home() {
-  return <></>;
+  return (
+    <Container maxWidth='lg'>
+      <Header
+        title='🔥 화려한 혈통 🔥'
+        sections={sections}
+      />
+    </Container>
+  );
 }
